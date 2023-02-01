b56d7c178ce1091e3ace256a3d517612643ea209
sagarv-vitara <89578890+sagarv-vitara@users.noreply.github.com>
2023-02-01 12:41:41 +0530

    updates

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ app.get("/", function (req, res) {
 });
 //epns channel => eip155:5:0xee6f3c8c7452AcafA0A2d2586552f8b77a3e4286
 app.use(function(req, res, next){
-    
+    if( ["http://localhost:4200", "https://jolly-wind-4771.on.fleek.co/"].indexOf(req.headers.origin) > -1){
+        res.setHeader("Access-Control-Allow-Origin",req.headers.origin);
+    }
+    res.set({
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type, token',
+        'Last-Modified': new Date().toUTCString()
+    });
+    if(req.method === "OPTIONS"){
+        return res.send(200);
+    }
+    next();
 })
 app.get('/songs', function(req, res){
     const songs = songInfo.list;
